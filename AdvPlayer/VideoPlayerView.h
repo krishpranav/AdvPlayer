5f9b75dc0d64f936a4a7a1b862fe7850b52756b4
krishpranav <krisna.pranav@gmail.com>
2021-07-16 16:17:31 +0530

    VideoPlayerView: videoplayerview

--- AdvPlayer/VideoPlayerView.h
+++ AdvPlayer/VideoPlayerView.h
@@ -6,3 +6,16 @@
 //  Copyright © 2021 krisna pranav. All rights reserved.
 //
 
+#import <Cocoa/Cocoa.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "EyeView.h"
+
+@class VideoPlayerViewProjectMethod;
+
+@interface VideoPlayerView : NSView
+
+- (void) loadURL: (NSURL *) movieURL projectionMethod: (VideoPlayerViewProjectionMethod *) projectionMethod;
+- (void) toggleFullscreen;
+
+@end
