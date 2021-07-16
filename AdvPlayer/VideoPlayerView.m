66fd5ede709d35be9f1c3baac49f7b212061ab86
krishpranav <krisna.pranav@gmail.com>
2021-07-16 16:24:51 +0530

    VideoPlayerView: observeValueForKeyPath

--- AdvPlayer/VideoPlayerView.m
+++ AdvPlayer/VideoPlayerView.m
@@ -57,4 +57,33 @@
     
 }
 
+- (void) observeValueForKeyPath: (NSString *) keyPath ofObject: (id) object change: (NSDictionary *) change context: (void *) context {
+    if(object == player) {
+        if([keyPath isEqualToString: @"currentItem.presentationSize"]) {
+            if(player.status == AVPlayerStatusReadyToPlay) {
+                
+                CGSize contentSize = player.currentItem.presentationSize;
+                
+                for(int i = 0; i < 2; i++) {
+                    
+                    AVPlayerLayer * playerLayer = [AVPlayerLayer playerLayerWithPlayer: player];
+                    playerLayer.videoGravity = AVLayerVideoGravityResize;
+                    playerLayer.frame = CGRectMake(0,0, contentSize.width, contentSize.height);
+                    
+                    CALayer * eyeViewLayer = [CALayer layer];
+                    eyeViewLayer.backgroundColor = [NSColor darkGrayColor].CGColor;
+                    
+                    EyeView * targetEyeView = (i == 0 ? leftView : rightView);
+                    _projectionMethod.eyeLayerHandler(eyeViewLayer, i, contentSize, playerLayer, targetEyeView);
+                    
+                    [eyeViewLayer addSublayer: playerLayer];
+                    
+                    targetEyeView.contents = eyeViewLayer;
+                    
+                }
+            }
+        }
+    }
+}
+
 @end
