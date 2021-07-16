c4cc1b80b1f27c46fa6f9fc13c805d726caf9a55
krishpranav <krisna.pranav@gmail.com>
2021-07-16 16:14:08 +0530

    video player

---
+++ AdvPlayer/VideoPlayerView.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerView.h
+//  AdvPlayer
+//
+//  Created by krisna pranav on 7/16/21.
+//  Copyright © 2021 krisna pranav. All rights reserved.
+//
+
+#ifndef VideoPlayerView_h
+#define VideoPlayerView_h
+
+
+#endif /* VideoPlayerView_h */
---
+++ AdvPlayer/VideoPlayerView.m
@@ -0,0 +1,9 @@
+//
+//  VideoPlayerView.m
+//  AdvPlayer
+//
+//  Created by krisna pranav on 7/16/21.
+//  Copyright © 2021 krisna pranav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
