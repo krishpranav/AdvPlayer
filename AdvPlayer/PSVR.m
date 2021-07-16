42ecbde20f9620318dc11c67331cb8877d210c68
krishpranav <krisna.pranav@gmail.com>
2021-07-16 16:10:39 +0530

    PSVR

--- AdvPlayer/PSVR.m
+++ AdvPlayer/PSVR.m
@@ -10,6 +10,7 @@
 #import "PSVR.h"
 #import <IOKit/hid/IOHIDLib.h>
 
+
 @interface PSVR (Private)
 - (void) _processHIDValue: (IOHIDValueRef) hidValue;
 @end
@@ -69,18 +70,35 @@ static void PSVR_HID_InputValueCallback(void * inContext, IOReturn inResult, voi
 NSString * const PSVRDataReceivedNotification = @"PSVRDataReceivedNotification";
 NSString * const PSVRDataReceivedNotificationDataKey = @"PSVRDataReceivedNotificationDataKey";
 
+
 @implementation PSVRData
 
 - (id) initWithData: (NSData *) data {
     if((self = [super init])) {
+        
         _rawData = data;
+        
     }
     return self;
-    
 }
 
 - (int16_t) yawAcceleration {
     return ([self readInt16: 20] + [self readInt16: 36]);
 }
 
+- (int16_t) pitchAcceleration {
+    return ([self readInt16: 22] + [self readInt16: 38]);
+}
+
+- (int16_t) rollAcceleration {
+    return ([self readInt16: 24] + [self readInt16: 40]);
+}
+
+- (int16_t) readInt16: (NSInteger) offset {
+    int16_t output;
+    [_rawData getBytes: &output range: NSMakeRange(offset, sizeof(output))];
+    return output;
+}
+
 @end
+
