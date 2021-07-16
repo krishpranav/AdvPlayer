2d07bb874b154fdf3e04eba8fe539c30f9dbf55f
krishpranav <krisna.pranav@gmail.com>
2021-07-16 15:49:54 +0530

    .

---
+++ AdvPlayer/PLSVR.h
@@ -0,0 +1,18 @@
+//
+//  PLSVR.h
+//  AdvPlayer
+//
+//  Created by krisna pranav on 7/16/21.
+//  Copyright © 2021 krisna pranav. All rights reserved.
+//
+
+// imports
+#import <Cocoa/Cocoa.h>
+
+@interface PSLVR : NSObject
+
++ (instancetype) sharedInstance;
+
++ (NSScreen *) screen;
+
+@end
---
+++ AdvPlayer/PSLVR.m
@@ -0,0 +1,8 @@
+//
+//  PSLVR.m
+//  AdvPlayer
+//
+//  Created by krisna pranav on 7/16/21.
+//  Copyright © 2021 krisna pranav. All rights reserved.
+//
+
