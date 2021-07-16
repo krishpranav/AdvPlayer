517be22b0ba946158ad0683fe1c3d139fe03654a
krishpranav <krisna.pranav@gmail.com>
2021-07-16 15:36:13 +0530

    implementation

---
+++ AdvPlayer/EyeView.m
@@ -0,0 +1,15 @@
+//
+//  EyeView.m
+//  AdvPlayer
+//
+//  Created by krisna pranav on 7/16/21.
+//  Copyright © 2021 krisna pranav. All rights reserved.
+//
+
+#import "EyeView.h"
+
+@implementation EyeView {
+    SCNNode * cameraNode;
+    SCNSphere * dome;
+    SCNNode * domeNode;
+}
